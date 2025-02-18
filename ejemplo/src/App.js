733397e0e922954abf3a0d9e3f52845f6513f481
sam <sam.corchero@enti.cat>
2025-02-18 20:58:37 +0100

    act1. exp y react

--- ejemplo/src/App.js
+++ ejemplo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenid@s a ejemplo!
         </p>
         <a
           className="App-link"
